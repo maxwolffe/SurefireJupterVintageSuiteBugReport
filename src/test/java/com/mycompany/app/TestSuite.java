ccccfe276aee503101fdeaa1a1d937ddddb4d80f
Max Wolffe <mwolffe@mwolffe-mn4.linkedin.biz>
2020-04-09 16:07:42 -0700

    Comment out failing test example

--- src/test/java/com/mycompany/app/TestSuite.java
+++ src/test/java/com/mycompany/app/TestSuite.java
@@ -12,6 +12,6 @@ public class TestSuite {
     @BeforeClass
     public static void setUp() {
         System.out.println("Running setup");
-        throw new RuntimeException("ex"); // Comment this out, and both surefire-junit-platform and surefire-junit47 pass the tests. 
+        // throw new RuntimeException("ex"); // Comment this out, and both surefire-junit-platform and surefire-junit47 pass the tests. 
     }
 }
